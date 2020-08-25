a271f4616b322257b3932bd17649fbc5f8296985
ranggabn <rbaghas32@gmail.com>
2020-08-25 21:47:20 +0700

    fourth commit

--- app/Http/Controllers/UploadController.php
+++ app/Http/Controllers/UploadController.php
@@ -1,31 +1,42 @@
 <?php
 
 namespace App\Http\Controllers;
+
 use App\Http\Controllers\Controller;
 use Spatie\PdfToImage\Pdf;
 use Org_Heigl\Ghostscript\Ghostscript;
 use thiagoalessio\TesseractOCR\TesseractOCR;
 use Illuminate\Http\Request;
 
-class UploadController extends Controller{
+class UploadController extends Controller
+{
     public function index()
     {
-        return view('index'); 
+        return view('index');
     }
 
     public function create(Request $request)
     {
         $pdfs = $request->file('uploadPDF')->store('public');
-        $pdf_file = storage_path('app/public').substr($pdfs,6);
-        $name = explode(".",substr($pdfs,7));
+        $pdf_file = storage_path('app/public') . substr($pdfs, 6);
+        $name = explode(".", substr($pdfs, 7));
         $output_path = storage_path("app/images//$name[0]-%d");
-        
+
         Ghostscript::setGsPath("C:\Program Files\gs\gs9.52\bin\gswin64c.exe");
         $pdf = new Pdf($pdf_file);
         $pdf->setOutputFormat('png')->saveImage($output_path);
-        
-        $tesseract = new TesseractOCR(public_path($output_path));
-        $text =  $tesseract->run();
-        echo $text;
+
+        $files = scandir(storage_path('app/images'));
+        $sorted = array();
+        foreach ($files as $file) {
+            $exploded = explode("-", $file);
+            if($exploded[0] == $name[0]){
+                array_push($sorted,$file);
+            }
+        }
+        foreach($sorted as $text){
+            $tesseract = new TesseractOCR(storage_path("app/images/").$text);
+            print($tesseract->run());
+        }
     }
 }
